f7d1630aaf77e5c14c05200ab98bd9cb8cdb6159
Shama <shamarokh@gmail.com>
2017-02-02 16:27:01 -0500

    lab3 done

--- app/src/main/java/com/example/shama/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/shama/androidlabs/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.shama.androidlabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class LoginActivity extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "StartActivity";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +38,109 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Log.i(ACTIVITY_NAME, "In onCreate()");
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    protected void onStart() {
+        super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+
+        Log.i(ACTIVITY_NAME, "In onStart()");
+
+        // final SharedPreferences prefs = getSharedPreferences("storeddata", Context.MODE_PRIVATE);
+        // prefs.getString("DefaultEmail","aradhita.mohanty@gmail.com");
+        //SharedPreferences.Editor writer = prefs.edit();
+        Button login_button = (Button) findViewById(R.id.button2);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+
+                EditText username = (EditText) findViewById(R.id.TE);
+                String key = getResources().getString(R.string.login_key);
+                String value = username.getText().toString();
+                editor.putString(key, value );
+                editor.commit();
+                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+        // Read from the shared preferences the value of the email
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
+        // get the default email from R and pass it in as default string returned by sharedPref.
+        String defaultEmail = getResources().getString(R.string.login);
+        String email = sharedPref.getString(getResources().getString(R.string.login_key), defaultEmail);
+
+        EditText username = (EditText) findViewById(R.id.TE);
+        username.setText(email);
+
+
+    }
+
+    // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+       // AppIndex.AppIndexApi.start(client, getIndexApiAction());
+   // }
+
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    protected void onStop() {
+        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+      //  AppIndex.AppIndexApi.end(client, getIndexApiAction());
+
+        Log.i(ACTIVITY_NAME, "In onStop()");
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    /*
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Login Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }*/
 }
--- app/src/main/java/com/example/shama/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/shama/androidlabs/StartActivity.java
@@ -1,13 +1,125 @@
 package com.example.shama.androidlabs;
 
+import android.app.Activity;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.content.Intent;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "StartActivity";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        Button b1 = (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, ListItemsActivity.class);
+                // startActivity(intent);
+                startActivityForResult(intent, 5);
+
+            }
+
+
+
+        } );
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 5 && resultCode== Activity.RESULT_OK){
+            Log.i(ACTIVITY_NAME, "Returns to StartActivity.onActivityResult");
+            String messgPassed = data.getStringExtra("Response");
+            CharSequence text = "ListItemsActivity Passed:"+messgPassed;
+            Toast toast = Toast.makeText(StartActivity.this,text,Toast.LENGTH_LONG);
+            toast.show();
+            Log.i(ACTIVITY_NAME,messgPassed);
+        }
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    protected void onStart() {
+        super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+
+        Log.i(ACTIVITY_NAME, "In onStart()");
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    protected void onStop() {
+        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+
+        Log.i(ACTIVITY_NAME, "In onStop()");
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Start Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
     }
 }
